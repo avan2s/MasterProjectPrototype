46d4f08eae0f501dbb0853cbc409baf642d52c21
Andy <Andy@192.168.2.121>
2015-11-13 12:02:18 +0100

    // Convert into Mixed Integer linear Programming

--- MasterProjectPrototype/src/Main.java
+++ MasterProjectPrototype/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import ilog.concert.*;
 import ilog.cplex.*;
 
@@ -5,9 +8,78 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Main.model1();
+		Main.exampleFarmer();
 	}
 
+	public static void exampleFarmer(){
+		try{
+			IloCplex cplex = new IloCplex();
+			double preisHafer = 30;
+			double preisGetreide = 40;
+			double hourHafer = 1;
+			double hourGetreide = 2;
+			int maxAcker = 240;
+			int maxStunden = 320;
+			
+			// variables
+			//IloNumVar x = cplex.numVar(0,Double.MAX_VALUE,"Getreide"); // x >= 0;
+			IloIntVar x = cplex.intVar(0, Integer.MAX_VALUE, "Getreide");
+			//IloNumVar y = cplex.numVar(0,Double.MAX_VALUE,"Hafer"); // y >= 0;
+			IloIntVar y = cplex.intVar(0, Integer.MAX_VALUE, "Hafer");
+			
+			// Objective-Funktion definieren
+			IloLinearNumExpr objective = cplex.linearNumExpr();
+			// 40x + 30y
+			objective.addTerm(preisGetreide, x);
+			objective.addTerm(preisHafer, y);
+			
+			// Minimierungsproblem
+			cplex.addMaximize(objective);
+			
+			
+			List<IloRange> constraints = new ArrayList<IloRange>();	
+			
+			// Constraints definieren
+			// x + y >= maximale zu bepflanzende Acker
+			IloLinearIntExpr cAcker = cplex.linearIntExpr();
+			cAcker.addTerm(1, x);
+			cAcker.addTerm(1, y);
+			constraints.add(cplex.addLe(cAcker,maxAcker));
+			System.out.println(constraints.get(0));
+			
+			// 2x + y >= maxStunden
+			IloLinearNumExpr cHours = cplex.linearNumExpr();
+			cHours.addTerm(hourGetreide,x);
+			cHours.addTerm(hourHafer,y);
+			constraints.add(cplex.addLe(cHours, maxStunden));
+			
+			//cplex.addLe(cplex.sum(cplex.prod(hourGetreide, x),cplex.prod(hourHafer, y)),maxStunden);
+			
+			// nur das Nötigste ausgeben
+			cplex.setParam(IloCplex.IntParam.SimDisplay, 0);
+			
+			// Parameter, ab wann eine Zahl als Integer gilt. 
+			// 2. Parameter Anzahl Kommastellen, erhöht aber stark die Laufzeit
+			// Standard ist: 10 Nachkommastellen müssen eine 0 vorweisen
+			//cplex.setParam(IloCplex.DoubleParam.EpInt, 0);
+			
+			// solve
+			if(cplex.solve()){
+				System.out.println("Wahrscheinlicher Umsatz = " + cplex.getObjValue());
+				System.out.println("x (Getreide) = " + Math.round(cplex.getValue(x)));
+				System.out.println("y (Hafer) = " + Math.round(cplex.getValue(y)));
+				
+			}
+			else{
+				System.out.println("Model not solved");
+			}
+			cplex.end();
+		}
+		catch(IloException ex){
+			ex.printStackTrace();
+		}
+	}
+	
 	public static void model1(){
 		try{
 			IloCplex cplex = new IloCplex();
@@ -37,8 +109,8 @@ public class Main {
 			// solve
 			if(cplex.solve()){
 				System.out.println("obj = " + cplex.getObjValue());
-				System.out.println("x = " + cplex.getValue(x));
-				System.out.println("y = " + cplex.getValue(y));
+				System.out.println("x (Getreide) = " + cplex.getValue(x));
+				System.out.println("y (Hafer) = " + cplex.getValue(y));
 			}
 			else{
 				System.out.println("Model not solved");
